35fbbfb34afb6e7521f3929a59957a0324cfd54f
MG Rabbi <golamrabbi3@gmail.com>
2024-04-27 20:17:35 +0100

    Testing and Fixing Profile Features

--- app/Http/Controllers/Api/Profile/EmailVerificationController.php
+++ app/Http/Controllers/Api/Profile/EmailVerificationController.php
@@ -1,28 +1,27 @@
 <?php
 
-namespace App\Http\Controllers\Api\Auth;
+namespace App\Http\Controllers\Api\Profile;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Auth\EmailVerificationRequest;
 use App\Mail\Api\Auth\EmailVerificationMail;
 use App\Mail\Api\Auth\EmailVerifiedMail;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
 class EmailVerificationController extends Controller
 {
-    public function index(Request $request): JsonResponse
+    public function index(): JsonResponse
     {
-        if ($request->user()->email_verified_at) {
+        if (request()->user()->email_verified_at) {
             return response()->json([
                 'message' => __('The email address is already verified.'),
             ], 409);
         }
 
         $OTP = random_int(100000, 999999);
-        Mail::to($request->user()->email)
-            ->send(new EmailVerificationMail($request->user()->name, $OTP));
+        Mail::to(request()->user()->email)
+            ->send(new EmailVerificationMail(request()->user()->name, $OTP));
 
         return response()->json([
             'message' => __('An email verification code has been sent to your email address.'),
@@ -42,7 +41,7 @@ class EmailVerificationController extends Controller
 
         if ($request->user()->update(['email_verified_at' => now()])) {
             Mail::to($request->user()->email)->send(new EmailVerifiedMail($request->user()->name));
-            
+
             return response()->json([
                 'message' => __('The email address has been verified successfully.'),
             ]);
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\Api\Auth\EmailVerificationController;
 use App\Http\Controllers\Api\Auth\LoginController;
 use App\Http\Controllers\Api\Auth\PasswordRecoveryController;
 use App\Http\Controllers\Api\Auth\RegisterController;
 use App\Http\Controllers\Api\News\GuestNewsController;
 use App\Http\Controllers\Api\News\NewsController;
+use App\Http\Controllers\Api\Profile\EmailVerificationController;
 use App\Http\Controllers\Api\Profile\LogoutController;
 use App\Http\Controllers\Api\Profile\PasswordController;
 use App\Http\Controllers\Api\Profile\ProfileController;
@@ -16,8 +16,6 @@ Route::prefix('v1')->group(function () {
     Route::resource('news', GuestNewsController::class)->only('index', 'show');
     Route::post('register', RegisterController::class);
     Route::post('login', [LoginController::class, 'login']);
-    Route::post('email-verification', [EmailVerificationController::class, 'index']);
-    Route::post('email-verification/verify', [EmailVerificationController::class, 'verify']);
     Route::post('password-recovery', [PasswordRecoveryController::class, 'index']);
     Route::post('password-recovery/recover', [PasswordRecoveryController::class, 'recover']);
 
@@ -26,6 +24,10 @@ Route::prefix('v1')->group(function () {
         Route::post('profile', [ProfileController::class, 'update']);
         Route::get('profile/logout', LogoutController::class);
         Route::post('profile/password', PasswordController::class);
+        Route::get('profile/email-verification', [EmailVerificationController::class, 'index'])
+            ->middleware('throttle:1,2');
+        Route::post('profile/email-verification', [EmailVerificationController::class, 'verify'])
+            ->middleware('throttle:3,1');
         Route::middleware('permission')->group(function () {
             Route::resource('roles', RolesController::class)->except('create', 'edit');
             Route::resource('news', NewsController::class)->except('create', 'edit');
