4a8cb47b658181bc3606d5d1563983902252a163
Md Golam Rabbi <golamrabbi3@gmail.com>
2024-08-08 21:18:02 +0600

    Integration VueTailAdmin in laravel

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,25 @@
-import axios from 'axios';
+import './assets/css/satoshi.css'
+import './assets/css/style.css'
+import 'jsvectormap/dist/jsvectormap.min.css'
+import 'flatpickr/dist/flatpickr.min.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import axios from 'axios'
+
+import VueApexCharts from 'vue3-apexcharts'
+
+import App from './App.vue'
+import router from './router'
+
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(VueApexCharts)
+
+app.mount('#app')
