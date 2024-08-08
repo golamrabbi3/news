885c98f196a0be64773ebf2a2108474091a13ece
Md Golam Rabbi <golamrabbi3@gmail.com>
2024-08-08 21:18:02 +0600

    Integration VueTailAdmin in laravel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
 import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url'
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
+        vue(),
+        vueJsx(),
     ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./resources/js', import.meta.url))
+        }
+    },
+    base: '',
+    server: {
+        host: "localhost",
+    },
 });
