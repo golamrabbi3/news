61e79532aac027cd5de9cf3bb66ed4f536e23ba3
Md Golam Rabbi <golamrabbi3@gmail.com>
2024-08-10 01:13:05 +0600

    Tweaking vite.config

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,9 +12,9 @@ import VueApexCharts from 'vue3-apexcharts'
 import App from './App.vue'
 import router from './router'
 
-window.axios = axios;
+window.axios = axios
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
 const app = createApp(App)
 
